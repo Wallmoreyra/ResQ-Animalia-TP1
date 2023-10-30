78c48d7a80438818e5c49ec117e3564605e3ff84
walter moreyra <49510922+Wallmoreyra@users.noreply.github.com>
2023-10-29 21:21:17 -0300

    JS de detalle y galeria
    terminado la parte de JS falta agregar estilos

--- pg-DetAnim/detalle.js
+++ pg-DetAnim/detalle.js
@@ -1,6 +1,8 @@
 function redirigir() {
     window.location.href = "../pg-Nav/aniCata.html";
-}
+};
+
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -17,11 +19,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.log(animal);
                 if (animal) {
                     document.getElementById('nombreAnimal').textContent ='Nombre: ' + animal.nombre;
-                    document.getElementById('imagenAnimal-00').src = animal.imagen['00'];
+                    document.getElementById('imagenAnimal-00').src = animal.imagen[0];
                     document.getElementById('edadAnimal').textContent = 'Edad: ' + animal.edad;
                     document.getElementById('localidadAnimal').textContent = 'Localidad: ' + animal.localidad;
                     document.getElementById('razaAnimal').textContent = 'Raza: ' + animal.raza;
-                    document.getElementById('tamañoHabitatAnimal').textContent = 'Tamaño de habitad: ' + animal.tamañoHabitad;
+                    document.getElementById('especieAnimal').textContent = 'Especie: ' + animal.especie;
+                    document.getElementById('vacunasAnimal').textContent = 'Vacunas: ' + animal.Vacunas;
+                    document.getElementById('EstirilizadoAnimal').textContent = 'Esterilizado: ' + animal.Estirilizado;
                     document.getElementById('cuidadosEspeciales').textContent = `Cuidados especiales: ${animal.cuidadosEspeciales}`;
                     document.getElementById('descripcionAnimal').textContent = `Descripción: ${animal.descripcion}`;
                 } else {
@@ -34,6 +38,48 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.log('No se recibió la ID del animal.');
     }
+});
+var img = 0;
+const imgSig = document.querySelector('.sigImg');
+imgSig.addEventListener('click', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const animalId = urlParams.get('id');
+    fetch('../api/db.json')
+            .then(response => response.json())
+            .then(data => {
+                const animal = data.animales.find(item => item.id == animalId);
+                var numImg = animal.imagen.length;
+                if(img < numImg - 1){
+                    img++;
+                } else {
+                    img = 0;
+                }
+                document.getElementById('imagenAnimal-00').src = animal.imagen[img];
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
+});
+
+const antImg = document.querySelector('.antImg');
+antImg.addEventListener('click', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const animalId = urlParams.get('id');
+    fetch('../api/db.json')
+            .then(response => response.json())
+            .then(data => {
+                const animal = data.animales.find(item => item.id == animalId);
+                var numImg1 = animal.imagen.length;
+                if(img == 0){
+                    img = numImg1 - 1;
+                } else {
+                    img--;
+                }
+                document.getElementById('imagenAnimal-00').src = animal.imagen[img];
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
 });
 
 
--- pg-Nav/listaDeAnimales.js
+++ pg-Nav/listaDeAnimales.js
@@ -1,9 +1,25 @@
+function filtradoPorRaza(datosAnimales, especie) {
+    return datosAnimales.animales.filter(animal => animal.especie === especie);
+};
+function cerrar() {
+    const iframe = document.getElementById('iframeContenedor');
+    iframe.style.display = 'none';
+    const cards = document.getElementById('cardConteiner');
+    cards.style.display = 'flex';
+    const botonesNav = document.getElementById('botonesPagNav');
+    botonesNav.style.display = 'block';
+    const botonCerrar = document.getElementById('botonCerrar');
+    botonCerrar.style.display = 'none';
+    const botonesFiltro = document.getElementById('botonesPagNav01');
+    botonesFiltro.style.display = 'block';
+};
 
 function generarCards(datos) {
+    
     const contenedor = document.querySelector('.contenedor');
     contenedor.innerHTML = '';
 
-    datos.animales.forEach(animal => {
+    datos.forEach(animal => {
         
         const card = document.createElement('div');
         card.className = 'card';
@@ -13,7 +29,7 @@ function generarCards(datos) {
         nombre.textContent = animal.nombre;
     
         const imagen = document.createElement('img');
-        imagen.src = animal.imagen['00'];
+        imagen.src = animal.imagen[0];
         imagen.alt = 'Imagen del animal';
     
         const racAge = document.createElement('div');
@@ -33,32 +49,160 @@ function generarCards(datos) {
         imagen.addEventListener('click', () => {
             const animalId = card.getAttribute('data-id');
             console.log(animalId)
-            window.location.href = `../pg-DetAnim/aniDet.html?id=${animalId}`;
+            //window.location.href = `../pg-DetAnim/aniDet.html?id=${animalId}`;
+            const iframe = document.getElementById('iframeContenedor');
+            const cards = document.getElementById('cardConteiner');
+            const botonesNav = document.getElementById('botonesPagNav');
+            const botonCerrar = document.getElementById('botonCerrar');
+            const botonesFiltro = document.getElementById('botonesPagNav01');
+            iframe.src= `../pg-DetAnim/aniDet.html?id=${animalId}`;
+            botonesNav.style.display = 'none';
+            cards.style.display = 'none';
+            iframe.style.display = 'block';
+            botonCerrar.style.display = 'block';
+            botonesFiltro.style.display = 'none';
+
         })
     
         contenedor.appendChild(card);
     });
-
+    
+    //console.log(datos.length);
 }
 
+//Cambiar cuando ya esten los estilos esta es la funcion permamente
+function inicioDePag(){
+    fetch('../api/db.json')
+            .then(response => response.json())
+            .then(datos => {
+                //console.log(datos.animales);
+                generarCards(datos.animales);
+                pagActual = 0;
+                verPag(pagActual);
+            })
+            .catch(error => {
+                console.error('Error al cargar los datos:', error);
+            });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const botonActualizar = document.querySelector('.botonActualizar');
+    botonActualizar.addEventListener('click', () => {
+        //alert("hola wachin!!")
+        fetch('../api/db.json')
+            .then(response => response.json())
+            .then(datos => {
+                //console.log(datos.animales);
+                generarCards(datos.animales);
+                pagActual = 0;
+                verPag(pagActual);
+            })
+            .catch(error => {
+                console.error('Error al cargar los datos:', error);
+            });
+        }
+    );
+});
 
+//Esta funcion esta momentanea mientras se agregan los estilos
+// const botonActualizar = document.querySelector('.botonActualizar');
+// botonActualizar.addEventListener('click', () => {
+//     //alert("hola wachin!!")
+//     fetch('../api/db.json')
+//         .then(response => response.json())
+//         .then(datos => {
+//             //console.log(datos.animales);
+//             generarCards(datos.animales);
+//             pagActual = 0;
+//             verPag(pagActual);
+//         })
+//         .catch(error => {
+//             console.error('Error al cargar los datos:', error);
+//         });
+//     }
+// );
 
-const botonActualizar = document.querySelector('.botonActualizar');
-botonActualizar.addEventListener('click', () => {
+const mostrarGatos = document.querySelector('.mostrarGatos');
+mostrarGatos.addEventListener('click', () => {
     //alert("hola wachin!!")
     fetch('../api/db.json')
         .then(response => response.json())
         .then(datos => {
-            console.log(datos);
-            generarCards(datos)
+            console.log(filtradoPorRaza(datos, "Gato"));
+            datos = filtradoPorRaza(datos, "Gato");
+            generarCards(datos);
+            pagActual = 0;
+            verPag(pagActual);
         })
         .catch(error => {
             console.error('Error al cargar los datos:', error);
         });
 });
+const mostrarPerros = document.querySelector('.mostrarPerros');
+mostrarPerros.addEventListener('click', () => {
+   
+    fetch('../api/db.json')
+        .then(response => response.json())
+        .then(datos => {
+            datos = filtradoPorRaza(datos, "Perro");
+            generarCards(datos);
+            pagActual = 0;
+            verPag(pagActual);
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
+});
+const mostrarOtros = document.querySelector('.mostrarOtros');
+mostrarOtros.addEventListener('click', () => {
+    
+    fetch('../api/db.json')
+        .then(response => response.json())
+        .then(datos => {
+            datos = filtradoPorRaza(datos, "Otro");
+            generarCards(datos);
+            pagActual = 0;
+            verPag(pagActual);
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
+        
+});
 
-// function actualizarLista() {
-//     console.log("hola wachin!!!")
-// };
+const cardPorPag = 6;
+let pagActual = 0;
+var cards = document.querySelectorAll('.card');
+const pagAnterior = document.querySelector('.pagina-anterior');
+const pagSiguiente = document.querySelector('.pagina-siguiente');
 
+
+function verPag(page) {
+    let cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        if (index >= page * cardPorPag && index < (page + 1) * cardPorPag){
+        card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
+
+
+pagAnterior.addEventListener('click', () => {
+    if (pagActual > 0) {
+        pagActual--;
+        verPag(pagActual);
+    }
+});
+
+pagSiguiente.addEventListener('click', () => {
+    console.log(cards.length);
+    cards = document.querySelectorAll('.card');
+    if (pagActual < Math.ceil(cards.length / cardPorPag) - 1) {
+        pagActual++;
+        verPag(pagActual);
+    }
+});
+inicioDePag();
+verPag(pagActual);
 
